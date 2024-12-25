cd2a53c6b719dc4153f768892a36cb5a45752207
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-12-25 21:22:40 +0200

    fooditems class added

--- Module-09(Restaurent_Management_System)/users.py
+++ Module-09(Restaurent_Management_System)/users.py
@@ -30,12 +30,19 @@ class Admin(User):
     def view_employee(self, restaurant):
         restaurant.view_employee()
 
+    def add_new_item(self, restaurant, item):
+        restaurant.menu.add_menu_items(item)
+
+    def remove_item(self, restaurant, item):
+        restaurant.menu.remove_item(item)
+
 
 class Restaurant:
 
     def __init__(self, name):
         self.name = name
         self.employees = []
+        self.menu = FoodItem()
 
     def add_employee(self, employee):
         self.employees.append(employee)
@@ -65,14 +72,42 @@ class Menu:
         item = self.find_item(item_name)
         if item:
             self.items.remove(item)
+            print(f"Item deleted")
+        else:
+            print("Item not found")
+
+    def show_menu(self):
+        print("*** Menu ***")
+        print("Name\tPrice\tQuantity")
+        for item in self.items:
+            print(f"{item.name} \t {item.price} \t {item.quantity}")
+
+
+class FoodItem:
+
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
 
 # emp = Employee("John", 171245478, "john@gmail.com", "Tampere, Finland", 33, "CEO", 4500)
 # print(emp.name)
 
 addAdmin = Admin("John", 171245478, "john@gmail.com", "Tampere, Finland")
-addAdmin.add_employee(
+restaurant = Restaurant("Tampere Diner")
+
+# Add employee
+employee = Employee(
     "Sami", 54546465, "josen@gmail.com", "Helsinki, Finland", 33, "CEO", 4500
 )
+addAdmin.add_employee(restaurant, employee)
 
-addAdmin.view_employee()
+# Add menu item
+mn = Menu()
+item = FoodItem("Pizza", 12, 1)
+mn.add_menu_items(item)
+mn.show_menu()
+
+# View employees
+addAdmin.view_employee(restaurant)
